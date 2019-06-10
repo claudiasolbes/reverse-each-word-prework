3e1fc27bf8578b62e557b35c8b4c5066ad1e9ae6
Claudia Solbes <cssolbes@hotmail.com>
2019-06-10 18:25:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   sentence = sentence1.split(" ")
-  sentence_reversed = sentence.collect do {|backwards_sentence| backwards_sentence.reverse!}
-  
+  sentence_reversed = sentence.collect {|backwards_sentence| backwards_sentence.reverse!}
+  sentence_reversed.join
   end
 end
